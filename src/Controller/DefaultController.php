656b54fa8c805c873ec58289bcd8f139dbc4c2f8
Victor Toledo <victortoledo@worldlex.net>
2024-02-25 11:51:31 +0100

    Feat: creado controlador default (cerrado #7)

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api')]
+
+class DefaultController extends AbstractController
+{
+    #[Route('/login', name: 'admin_login')]
+    public function adminLogin(): Response
+    {
+        return $this->render('Admin/AdminLogin.html.twig', [
+            'test' => 'x',
+        ]);
+    }
+
+
+
+    #[Route('/test', name: 'test', methods: ['GET'])]
+    public function test()
+    {
+        return new JsonResponse('holi chuli', Response::HTTP_OK);
+    }
+}
