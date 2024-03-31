d54ff272720c1ac015e8bd286238856eda051835
Victor Toledo <victortoledo@worldlex.net>
2024-03-31 11:16:15 +0200

    añadida seguridad entre otros

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,25 +2,38 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse as HttpFoundationJsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\test\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api')]
 
 class DefaultController extends AbstractController
 {
-    #[Route('/login', name: 'admin_login')]
-    public function adminLogin(): Response
-    {
-        return $this->render('Admin/AdminLogin.html.twig', [
-            'test' => 'x',
-        ]);
+    private $request;
+    private $authenticationUtils;
+    private $em;
+
+    
+    private $validator;
+
+    function __construct(
+        RequestStack $requestStack,
+        AuthenticationUtils $authenticationUtils,
+        EntityManagerInterface $entityManagerInterface,
+        
+        ValidatorInterface $validatorInterface
+    ) {
+        $this->em = $entityManagerInterface;
+        $this->request = $requestStack->getCurrentRequest();
+        $this->validator = $validatorInterface;
     }
-
-
 
     #[Route('/test', name: 'test', methods: ['GET'])]
     public function test()
