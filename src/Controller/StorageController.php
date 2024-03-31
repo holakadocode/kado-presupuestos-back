5d302504993ac3b13dca0ff2ae3d54f8b3d78df3
Jose Arozena <j.arocenap@gmail.com>
2024-03-31 10:44:21 +0200

    terminado StorageController

--- src/Controller/StorageController.php
+++ src/Controller/StorageController.php
@@ -73,22 +73,67 @@ class StorageController extends AbstractController
         $data = json_decode($this->request->getContent(), true);
 
         $familyFolder = $this->em->getRepository('App/Entity/FamilyFolder')->findOneById($data['id']);
-        $budgetArticle = $this->em->getRepository('App/Entity/BudgetArticle')->findOneById($data['budgetArticleId']);
 
         $newArticle = new Article();
-        $newArticle->setFamilyFolder($familyFolder);
-        $newArticle->setDateTime($data["date"]);
-        $newArticle->setName($data["name"]);
-        $newArticle->setDescription($data["description"]);
-        $newArticle->setCode($data["code"]);
-        $newArticle->setDistributorCode($data["distributorCode"]);
-        $newArticle->setDistributorPrice($data["distributorPrice"]);
-        $newArticle->setPrice($data["articlePrice"]);
-        $newArticle->setBudgetArticle($budgetArticle);
+        $newArticle
+        ->setFamilyFolder($familyFolder)
+        ->setDateTime(new \DateTime())
+        ->setName($data["name"])
+        ->setDescription($data["description"])
+        ->setCode($data["code"])
+        ->setDistributorCode($data["distributorCode"])
+        ->setDistributorPrice($data["distributorPrice"])
+        ->setPrice($data["articlePrice"]);
 
         $this->em->persist($newArticle);
         $this->em->flush();
 
-        return new JsonResponse($data["nombreCarpeta"], Response::HTTP_OK);
+        return new JsonResponse("", Response::HTTP_OK);
+    }
+
+    #[Route('/delete/{articleID}', name: 'api_article_delete', methods: ['DELETE'])]
+    public function api_article_delete($articleID): Response
+    {
+
+        //$data = json_decode($this->request->getContent(), true);
+
+        // if (!$data || !isset($data['articleID'])) {
+        //     return new JsonResponse('Invalid article data', Response::HTTP_BAD_REQUEST);
+        // }
+
+        $article = $this->em->getRepository('App\Entity\Article')->findOneById($articleID);
+
+        if (!$article) {
+            return new JsonResponse('Article not found', Response::HTTP_NOT_FOUND);
+        }
+
+        $this->em->remove($article);
+        $this->em->flush();
+
+        return new JsonResponse('Article deleted', Response::HTTP_OK);
+    }
+
+    #[Route('/edit/{articleID}', name: 'api_storage_edit', methods: ['POST'])]
+    public function api_storage_edit($articleID): Response
+    {
+        $data = json_decode($this->request->getContent(), true);
+
+        $familyFolder = $this->em->getRepository('App/Entity/FamilyFolder')->findOneById($data['id']);
+
+        $articleToEdit = $this->em->getRepository('App/Entity/Article')->findOneById($articleID);
+
+        $articleToEdit
+            ->setFamilyFolder($familyFolder)
+            ->setDateTime(new \DateTime())
+            ->setName($data["payload"]["name"])
+            ->setDescription($data["payload"]["description"])
+            ->setCode($data["payload"]["code"])
+            ->setDistributorCode($data["payload"]["distributorCode"])
+            ->setDistributorPrice($data["payload"]["distributorPrice"])
+            ->setPrice($data["payload"]["articlePrice"]);
+
+        $this->em->flush();
+
+        return new JsonResponse('Client edited', Response::HTTP_OK);
     }
 }
