dcd5e86d4e715ffde3210d0b890cbdd9f55f2e74
bibs <bibsbiana@protonmail.com>
2024-04-01 16:17:55 +0200

    feat excel

--- src/Controller/ExcelController.php
+++ src/Controller/ExcelController.php
@@ -32,20 +32,18 @@ class ExcelController extends AbstractController
 
 
 
-    #[Route('/test', name: 'testxls')]
-    public function test(ParameterBagInterface $params)
+    #[Route('/budgetXls', name: 'budgetXls')]
+    public function budgetXls(ParameterBagInterface $params)
     {
-        // Create the spreadsheet
         $spreadsheet = new Spreadsheet();
         $spreadsheet->getDefaultStyle()->applyFromArray([
             'font' => [
-                'size' => 12,
+                'size' => 10,
                 'name' => 'Arial'
             ]
         ]);
 
-        // Sheet title
-        $sheet = $spreadsheet->getActiveSheet()->setTitle('Portada');
+        $sheet = $spreadsheet->getActiveSheet()->setTitle('budget.title'); //////////////////////CODIGO PRESUPEUSTO MEJOR
 
         // Borders
         $topBorderThinStyle = ['borders' => ['top' => ['borderStyle' => Border::BORDER_THIN, 'color' => ['rgb' => '000000']]]];
@@ -60,154 +58,124 @@ class ExcelController extends AbstractController
                 ],
             ],
         ];
-        $bordersThickStyle = [
-            'borders' => [
-                'allBorders' => [
-                    'borderStyle' => Border::BORDER_THICK,
-                    'color' => ['argb' => 'FF000000'],
-                ],
-            ],
-        ];
-        // Merge
-        $sheet->mergeCells([4, 15, 8, 18]);
 
-        // Borders
-        $sheet->getStyle([2, 2, 10, 2])->applyFromArray($topBorderThinStyle);
-        $sheet->getStyle([10, 2, 10, 37])->applyFromArray($rightBorderThinStyle);
-        $sheet->getStyle([2, 37, 10, 37])->applyFromArray($bottomBorderThinStyle);
-        $sheet->getStyle([2, 2, 2, 37])->applyFromArray($leftBorderThinStyle);
-        $sheet->getStyle([2, 2, 10, 37])->getFill()->setFillType('solid')->getStartColor()->setRGB('ffffff');
-        $sheet->getStyle([4, 15, 8, 18])->applyFromArray($bordersThickStyle);
-
-        // Width
-        $sheet->getColumnDimensionByColumn(2)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(3)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(4)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(5)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(6)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(7)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(8)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(9)->setWidth(12);
-        $sheet->getColumnDimensionByColumn(10)->setWidth(12);
-
-        // General style
-        $sheet->getStyle([4, 15])->getAlignment()->setHorizontal('center');
-        $sheet->getStyle([4, 15])->getAlignment()->setVertical('center');
-        $sheet->getStyle([4, 15])->getFill()->setFillType('solid')->getStartColor()->setRGB('373837');
-        $sheet->getStyle([4, 15])->getFont()->setSize(24);
-        $sheet->getStyle([4, 15])->getFont()->setBold(true);
-        $sheet->getStyle([4, 15])->getFont()->getColor()->setRGB('ffffff');
-
-        $sheet->getStyle([5, 20])->getFont()->setSize(9);
-        $sheet->getStyle([5, 22])->getFont()->setSize(9);
-
-        // DATOS
-        $client = $this->em->getRepository('App\Entity\Client')->findOneById(1);
-        
-        // Values
-        $sheet
-            ->setCellValue([4, 15], 'Presupuesto')
-            ->setCellValue([4, 20], 'Cliente: ')
-            ->setCellValue([5, 20], $client->getName().' '.$client->getSurname())
-            ->setCellValue([4, 22], 'Fecha: ')
-            ->setCellValue([5, 22], 'fecha ??');
-
-        // Create another sheet
-        $spreadsheet->createSheet(1);
-        $spreadsheet->setActiveSheetIndex(1);
-        $sheet = $spreadsheet->getActiveSheet()->setTitle('budget.title');
-
-        $spreadsheet->getDefaultStyle()->applyFromArray([
-            'font' => [
-                'size' => 10,
-                'name' => 'Arial'
-            ]
-        ]);
 
         // Set height and width
+        $sheet->getRowDimension(3)->setRowHeight(32);
+        $sheet->getRowDimension(4)->setRowHeight(22);
+        $sheet->getRowDimension(5)->setRowHeight(22);
         $sheet->getRowDimension(6)->setRowHeight(22);
+        $sheet->getRowDimension(7)->setRowHeight(22);
+        $sheet->getRowDimension(10)->setRowHeight(22);
+
         $sheet->getColumnDimensionByColumn(1)->setWidth(15);
         $sheet->getColumnDimensionByColumn(2)->setWidth(50);
-        $sheet->getColumnDimensionByColumn(3)->setWidth(12);
+        $sheet->getColumnDimensionByColumn(3)->setWidth(15);
         $sheet->getColumnDimensionByColumn(4)->setWidth(15);
         $sheet->getColumnDimensionByColumn(5)->setWidth(15);
 
-        // Table header
-        $sheet->getStyle([1, 6, 5, 6])->applyFromArray($bordersThinStyle);
-        $sheet->getStyle([1, 6, 5, 6])->getAlignment()->setVertical('center');
-        $sheet->getStyle([1, 6, 5, 6])->getAlignment()->setHorizontal('center');
-        $sheet->getStyle([1, 6, 5, 6])->getFill()->setFillType('solid')->getStartColor()->setRGB('373837');
-        $sheet->getStyle([1, 6, 5, 6])->getFont()->setBold(true);
-        $sheet->getStyle([1, 6, 5, 6])->getFont()->getColor()->setRGB('ffffff');
+        // Header
+        // $sheet->getStyle([1, 4, 5, 4])->getAlignment()->setVertical('center');
+        $sheet->getStyle([1, 3, 5, 7])->getAlignment()->setHorizontal('left');
+        $sheet->getStyle([1, 3, 5, 7])->getAlignment()->setVertical('center');
+
+        //HeaderCliente
+        $sheet->mergeCells([1, 3, 2, 3]);
+        $sheet->mergeCells([1, 4, 2, 4]);
+        $sheet->mergeCells([1, 5, 2, 5]);
+        $sheet->mergeCells([1, 5, 2, 5]);
+        $sheet->mergeCells([1, 6, 2, 6]);
+        $sheet->mergeCells([1, 7, 2, 7]);
+
+        //Header Presupuesto
+        $sheet->mergeCells([3, 3, 5, 3]);
+        $sheet->mergeCells([3, 4, 5, 4]);
+        $sheet->mergeCells([3, 5, 5, 5]);
+        $sheet->mergeCells([3, 6, 5, 6]);
+        $sheet->mergeCells([3, 7, 5, 7]);
+
+
+        $sheet->getStyle([1, 3, 5, 3])->getFont()->setBold(true);
+        $sheet->getStyle([1, 3, 5, 3])->getFont()->setSize(14);
+
+        //Tabla
+        $sheet->getStyle([1, 10, 5, 10])->getFill()->setFillType('solid')->getStartColor()->setRGB('373837');
+        $sheet->getStyle([1, 10, 5, 10])->getFont()->getColor()->setRGB('ffffff');
+
+
+
+        // DATOS ////
+
+        $data = json_decode($this->request->getContent(), true);
+
+        $budget = $this->em->getRepository('App\Entity\Budget')->findOneById($data['budgetID']);
+        $client = $this->em->getRepository('App\Entity\Client')->findOneById($data['clientID']);
 
         $sheet
-            ->setCellValue([1, 6], 'Código')
-            ->setCellValue([2, 6], 'Artículo')
-            ->setCellValue([3, 6], 'Cantidad')
-            ->setCellValue([4, 6], 'Precio unidad')
-            ->setCellValue([5, 6], 'Total');
+            ->setCellValue([1, 3], 'Cliente')
+            ->setCellValue([1, 4], $client->getName())
+            ->setCellValue([1, 5], $client->getTaxIdentification())
+            ->setCellValue([1, 6], "{$client->getAddress()} - {$client->getCity()}")
+            ->setCellValue([1, 7], "{$client->getTlf()} - {$client->getContactEmail()}")
 
-    
+            ->setCellValue([3, 3], 'Presupuesto')
+            // ->setCellValue([3, 4], $budget->getCode())   /////////////////////////////////////////////
+            ->setCellValue([3, 4], 'CODIGO ????')
+            ->setCellValue([3, 5], $budget->getDateTime())
+
+            ->setCellValue([1, 10], 'Código')
+            ->setCellValue([2, 10], 'Artículo')
+            ->setCellValue([3, 10], 'Cantidad')
+            ->setCellValue([4, 10], 'Precio')
+            ->setCellValue([5, 10], 'Total');
+
 
         $total = 0;
-        $currentRow = 7;
+        $currentRow = 11;
 
-        foreach ($client->getBudgets() as $budget) {
+        foreach ($budget->getBudgetArticles() as $article) {   //////////////////////////////////////
             $sheet->getRowDimension($currentRow)->setRowHeight(22);
 
             $sheet
-                // ->setCellValue([1, $currentRow], $budget['id']) método JAIME
-                ->setCellValue([1, $currentRow], $budget->getId())
-                ->setCellValue([2, $currentRow], $budget->getTitle())
-                ->setCellValue([3, $currentRow], '2')
-                ->setCellValue([4, $currentRow], '600 €')
-                ->setCellValue([5, $currentRow], $budget->getTotal());
+                ->setCellValue([1, $currentRow], $article->getArticleCode())
+                ->setCellValue([2, $currentRow], $article->getNameArticle())
+                ->setCellValue([3, $currentRow], $article->getQuantity())
+                ->setCellValue([4, $currentRow], $article->getPrice())
+                ->setCellValue([5, $currentRow], $article->getTotal());
 
-            $total=$total + $budget->getTotal();
+            $total = $total + $article->getTotal();
             $currentRow++;
         }
         $currentRow--;
 
-
         // Table content
-        $sheet->getStyle([5, 7, 5, $currentRow])->applyFromArray($rightBorderThinStyle);
+        $sheet->getStyle([5, 11, 5, $currentRow])->applyFromArray($rightBorderThinStyle);
         $sheet->getStyle([1, $currentRow, 5, $currentRow])->applyFromArray($bottomBorderThinStyle);
-        $sheet->getStyle([1, 7, 5, $currentRow])->getAlignment()->setVertical('center');
-        $sheet->getStyle([1, 7, 1, $currentRow])->getAlignment()->setHorizontal('center');
-        $sheet->getStyle([3, 7, 5, $currentRow])->getAlignment()->setHorizontal('center');
-        // $sheet->getStyle([3, 7, 5, 9])->getAlignment()->setWrapText(true);   -----------------
+        $sheet->getStyle([1, 10, 5, $currentRow])->getAlignment()->setVertical('center');
+        $sheet->getStyle([1, 10, 5, $currentRow])->getAlignment()->setHorizontal('center');
 
+        //Tabla IVA
+        $sheet->getRowDimension($currentRow + 4)->setRowHeight(22);
+        $sheet->getRowDimension($currentRow + 5)->setRowHeight(22);
+        $sheet->getStyle([1, $currentRow + 4, 5, $currentRow + 4])->getFill()->setFillType('solid')->getStartColor()->setRGB('373837');
+        $sheet->getStyle([1, $currentRow + 4, 5, $currentRow + 4])->getFont()->getColor()->setRGB('ffffff');
 
+        $sheet
+            ->setCellValue([2, $currentRow + 4], 'IVA %    ')
+            ->setCellValue([2, $currentRow + 5], '21%')
+            ->setCellValue([3, $currentRow + 4], 'IVA TOTAL')
+            ->setCellValue([3, $currentRow + 5], '????')  /////////////////////////////////////////////////////////////
+            ->setCellValue([4, $currentRow + 4], 'SUBTOTAL')
+            ->setCellValue([4, $currentRow + 5], '????')  ///////////////////////////////////////////////////////////////
+            ->setCellValue([5, $currentRow + 4], 'TOTAL')
+            ->setCellValue([5, $currentRow + 5], $total);
 
-        // Observations
-        $sheet->getStyle([2, $currentRow + 4])->applyFromArray($topBorderThinStyle);
-        $sheet->getStyle([2, $currentRow + 4, 2, $currentRow + 8])->applyFromArray($rightBorderThinStyle);
-        $sheet->getStyle([2, $currentRow + 8])->applyFromArray($bottomBorderThinStyle);
-        $sheet->getStyle([2, $currentRow + 4, 2, $currentRow + 8])->applyFromArray($leftBorderThinStyle);
-        $sheet->getStyle([2, $currentRow + 4, 2, $currentRow + 8])->getFill()->setFillType('solid')->getStartColor()->setRGB('ffffff');
-        $sheet->getStyle([2, $currentRow + 4])->getFont()->setBold(true);
+        $sheet->getStyle([5, $currentRow + 4, 5, $currentRow + 5])->applyFromArray($rightBorderThinStyle);
+        $sheet->getStyle([1, $currentRow + 4, 5, $currentRow + 5])->applyFromArray($bottomBorderThinStyle);
+        $sheet->getStyle([2, $currentRow + 4, 5, $currentRow + 5])->getAlignment()->setVertical('center');
+        $sheet->getStyle([2, $currentRow + 4, 2, $currentRow + 5])->getAlignment()->setHorizontal('right');
+        $sheet->getStyle([3, $currentRow + 4, 5, $currentRow + 5])->getAlignment()->setHorizontal('center');
 
-        $sheet->setCellValue([2, $currentRow + 4], 'Observaciones:');
-        $sheet->mergeCells([2, $currentRow + 5, 2, $currentRow + 8]);
-
-
-        // Observations
-        $sheet->getStyle([4, $currentRow + 4, 5, $currentRow + 4])->applyFromArray($topBorderThinStyle);
-        $sheet->getStyle([5, $currentRow + 4, 5, $currentRow + 8])->applyFromArray($rightBorderThinStyle);
-        $sheet->getStyle([4, $currentRow + 8, 5, $currentRow + 8])->applyFromArray($bottomBorderThinStyle);
-        $sheet->getStyle([4, $currentRow + 4, 4, $currentRow + 8])->applyFromArray($leftBorderThinStyle);
-        $sheet->getStyle([4, $currentRow + 4, 5, $currentRow + 8])->getFill()->setFillType('solid')->getStartColor()->setRGB('ffffff');
-        $sheet->getStyle([4, $currentRow + 8, 5, $currentRow + 8])->getFont()->setBold(true);
-        $sheet->getStyle([4, $currentRow + 4, 5, $currentRow + 8])->getAlignment()->setHorizontal('center');
-
-        $sheet->setCellValue([4, $currentRow + 4], 'IVA');
-        $sheet->setCellValue([5, $currentRow + 4], '??? %');
-        $sheet->setCellValue([4, $currentRow + 5], 'IVA TOTAL');
-        $sheet->setCellValue([5, $currentRow + 5], '??? €');
-        $sheet->setCellValue([4, $currentRow + 6], 'Subtotal');
-        $sheet->setCellValue([5, $currentRow + 6], '??? €');
-        $sheet->setCellValue([4, $currentRow + 8], 'TOTAL');
-        $sheet->setCellValue([5, $currentRow + 8], $total. ' €');
 
         $spreadsheet->setActiveSheetIndex(0);
 
